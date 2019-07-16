4a58bf19bab7e7dedb5cb49a52ded12b7fc65b6d
Shelby McCowan <smccowan33@gmail.com>
2019-07-16 16:47:37 -0600

    Just ending the day here...don't judge my poor commits

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ElectronService } from '../../providers/electron.service'
+import { isDevMode } from '@angular/core';
+import * as fs from 'fs';
+import * as path from 'path'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  pathText: string;
 
-  constructor() { }
+  filePath: string;
+
+  constructor(public electron: ElectronService) {
+    console.log(isDevMode());
+  }
 
   ngOnInit() {
+    if (isDevMode()){
+      console.log(this.electron.remote.app.getAppPath())
+      this.pathText = this.electron.remote.app.getAppPath()
+    }else{
+      this.pathText = process.env.PORTABLE_EXECUTABLE_DIR
+    }
+
+    if (!fs.existsSync(this.pathText + "/sheets")){
+      fs.mkdirSync(this.pathText + "/sheets");
+    }
   }
 
 }
