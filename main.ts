506797657e726969335d3c6570738685ded1a07b
Shelby McCowan <smccowan33@gmail.com>
2019-07-17 08:19:29 -0600

    Felt like changing window size, might change it later, idk

--- main.ts
+++ main.ts
@@ -15,8 +15,8 @@ function createWindow() {
   win = new BrowserWindow({
     x: 0,
     y: 0,
-    width: size.width,
-    height: size.height,
+    width: 720,
+    height: 450,
     webPreferences: {
       nodeIntegration: true,
     },
