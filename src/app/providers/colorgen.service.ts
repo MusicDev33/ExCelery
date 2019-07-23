922113617cc8223de2e4efbfe34b70129bb03609
Shelby McCowan <smccowan33@gmail.com>
2019-07-23 16:39:39 -0600

    Added pre-defined colors that should look good no matter what

--- src/app/providers/colorgen.service.ts
+++ src/app/providers/colorgen.service.ts
@@ -5,5 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class ColorgenService {
 
+  colors = ["#5496ff", "#0457db", "#07defa", "#00e8b2", "#02e665", "#1fff3d", "#a4f207",
+   "#e6da00", "#e69200", "#ffc45e" ,"#d47202", "#f54f02", "#e60b07" ,"#ff9b99", "#9f8cff",
+   "#2500e3", "#a142ff", "#6400c7", "#d000ff", "#ff00c8"]
+
   constructor() { }
+
+  generateRandomColor(floor, ceiling){
+    var color = (Math.random()*0xFFFFFF<<0).toString(16);
+    return '#'+ color;
+  }
 }
