9ff14741889687325856c75960aad02e4bc163ed
Shelby McCowan <smccowan33@gmail.com>
2019-07-24 11:24:32 -0600

    Keys and selected headers work finally

--- src/app/components/copymode/copymode.component.ts
+++ src/app/components/copymode/copymode.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ElectronService } from '../../providers/electron.service'
 import { ExcelService, ExcelFile } from '../../providers/excel.service'
+import { ColorgenService } from '../../providers/colorgen.service'
 import { Borders, FillPattern, Font, Workbook, Worksheet } from 'exceljs';
 import { Subscription } from 'rxjs';
 
@@ -38,9 +39,15 @@ export class CopymodeComponent implements OnInit, OnDestroy {
 
   rowObjectsDict: any = {}
 
+  colorsDict: any = {};
+  columnMap: any = {};
+  selectedHeader: string = ''
+  mappedHeader: string = ''
+
   constructor(
     public electron: ElectronService,
-    public excel: ExcelService) { }
+    public excel: ExcelService,
+    public colorGen: ColorgenService) { }
 
   ngOnInit() {
     // wb is an excel file interface
@@ -69,15 +76,78 @@ export class CopymodeComponent implements OnInit, OnDestroy {
     }
   }
 
-  selectTitleClicked(title: string){
-    this.activeText = ""
-    if (this.selectedTitles.includes(title)){
-      const index = this.selectedTitles.indexOf(title, 0);
+  // Fuzzy logic that will hopefully be fixed soon
+  checkMarkClicked(filename, header){
+    if (this.primaryKey == '' && this.secondaryKey == ''){
+      return
+    }
+    /*
+    if (this.selectedTitles.includes(header)){
+      const index = this.selectedTitles.indexOf(header, 0);
       if (index > -1) {
          this.selectedTitles.splice(index, 1);
       }
     }else{
-      this.selectedTitles.push(title)
+      this.selectedTitles.push(header)
+      this.mapColor(filename, header)
+    }*/
+    console.log(this.headerToColumn)
+    if (this.getKeyFile(filename) == 1){
+      if (this.selectedHeader == this.createKey(filename, header)){
+        this.selectedHeader = ''
+        return
+      }
+      this.selectedHeader = this.createKey(filename, header)
+    }else if(this.getKeyFile(filename) == 2){
+      if (this.mappedHeader == this.createKey(filename, header)){
+        this.mappedHeader = ''
+        return
+      }
+      this.mappedHeader = this.createKey(filename, header)
+    }
+  }
+
+  isSelected(filename, header){
+    return this.createKey(filename, header) == this.selectedHeader || this.createKey(filename, header) == this.mappedHeader;
+  }
+
+  copyColumns(){
+    console.log(this.rowObjectsDict)
+    var index = 1
+    for (let rowNum in this.rowObjectsDict[this.primaryKey.split(":")[0]]){
+      var primaryKeyValue = this.rowObjectsDict[this.primaryKey.split(":")[0]][rowNum][this.primaryKey.split(":")[1]]
+
+      var value = this.rowObjectsDict[this.secondaryKey.split(":")[0]].filter(rowObj => {
+        if (rowObj[this.secondaryKey.split(":")[1]].hasOwnProperty("result")){
+          return rowObj[this.secondaryKey.split(":")[1]]["result"] == primaryKeyValue;
+        }else{
+          return rowObj[this.secondaryKey.split(":")[1]] == primaryKeyValue;
+        }
+      })
+      //console.log(this.rowObjectsDict[this.primaryKey.split(":")[0]][rowNum][this.selectedHeader.split(":")[1]])
+      if (value.length){
+        console.log(index)
+        console.log(value)
+        index += 1
+      }
+    }
+  }
+
+  getRandomColor(){
+    return this.colorGen.colors[Math.floor(Math.random()*this.colorGen.colors.length)];
+  }
+
+  mapColor(filename, header){
+    this.colorsDict[this.createKey(filename, header)] = this.colorGen.colors[Math.floor(Math.random()*this.colorGen.colors.length)];
+    console.log(this.colorsDict)
+  }
+
+  getColor(filename, header){
+    var key = this.createKey(filename, header)
+    if (this.colorsDict.hasOwnProperty(key)) {
+      return this.colorsDict[key]
+    }else{
+      return "#666"
     }
   }
 
@@ -90,6 +160,10 @@ export class CopymodeComponent implements OnInit, OnDestroy {
     var index = this.openWorkbooks.findIndex(x => x.filename === filename);
     if (index !== -1) this.openWorkbooks.splice(index, 1);
     delete this.rowObjectsDict[filename]
+    this.primaryKey = ''
+    this.secondaryKey = ''
+    this.selectedHeader = ''
+    this.mappedHeader = ''
   }
 
   createKey(filename, key){
@@ -133,6 +207,26 @@ export class CopymodeComponent implements OnInit, OnDestroy {
 
   getIfSecondaryKey(filename, key){
     return this.secondaryKey === this.createKey(filename, key)
+  }
+
+  getKeyType(filename, header){
+    if (this.getIfPrimaryKey(filename, header)){
+      return 1;
+    }else if(this.getIfSecondaryKey(filename, header)){
+      return 2
+    }else{
+      return 0;
+    }
+  }
+
+  getKeyFile(filename){
+    if (this.primaryKey.includes(filename)){
+      return 1;
+    }else if(this.secondaryKey.includes(filename)){
+      return 2
+    }else{
+      return 0;
+    }
   }
 
   deleteKey(filename, key){
