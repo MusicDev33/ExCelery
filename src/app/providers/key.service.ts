0c6ab40ffeaa91cdefc3161621e6802a54d7fdde
Shelby McCowan <smccowan33@gmail.com>
2019-07-31 11:40:54 -0600

    Added methods to key service

--- src/app/providers/key.service.ts
+++ src/app/providers/key.service.ts
@@ -4,19 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class KeyService {
-  primaryKey: string;
-  secondaryKey: string;
+  primaryKey = '';
+  secondaryKey = '';
 
-  primaryFile: string;
-  primaryHeader: string;
+  primaryFile = '';
+  primaryHeader = '';
 
-  secondaryFile: string;
-  secondaryHeader: string;
+  secondaryFile = '';
+  secondaryHeader = '';
 
   constructor() { }
 
   createKey(filename: string, header: string) {
     return filename + ':' + header;
+  }
+
+  setKey(filename: string, header: string) {
+    if (this.getIfKey(filename, key)) {
+      this.deleteKey(filename, key);
+      return;
+    }
+
+    if (this.primaryKey !== '' && this.secondaryKey === '') {
+      this.secondaryKey = this.keyService.createKey(filename, key);
+    } else if (this.primaryKey === '') {
+      this.primaryKey = this.keyService.createKey(filename, key);
+    }
   }
 
   setPrimaryKey(filename: string, header: string) {
@@ -29,5 +42,59 @@ export class KeyService {
     this.secondaryKey = this.createKey(filename, header);
     this.secondaryFile = filename;
     this.secondaryHeader = header;
+  }
+
+  doBothKeysExist() {
+    return this.primaryKey.length > 0 && this.secondaryKey.length > 0;
+  }
+
+  getIsPrimaryKey(filename: string, header: string) {
+    return this.primaryKey === this.createKey(filename, header);
+  }
+
+  getIsSecondaryKey(filename: string, header: string) {
+    return this.secondaryKey === this.createKey(filename, header);
+  }
+
+  keyExists(filename: string, header: string) {
+    return this.getIsPrimaryKey(filename, header) || this.getIsSecondaryKey(filename, header);
+  }
+
+  getWhichKey(filename: string, header: string) {
+    if (!this.keyExists(filename, header)) {
+      return 0;
+    }
+    // Still not sure about readability here
+    return this.getIsPrimaryKey(filename, header) ? 1 : 2;
+  }
+
+  getWhichKeyFileIn(filename) {
+    if (this.isFilenameInKey(filename)) {
+      return 0;
+    }
+    // Again, might change for readability's sake
+    return this.primaryIncludes(filename) ? 1 : 2;
+  }
+
+  isFilenameInKey(filename: string) {
+    return this.primaryKey.includes(filename) || this.secondaryKey.includes(filename);
+  }
+
+  // DELETE Methods
+  deleteKeys() {
+    this.deletePrimaryKey();
+    this.deleteSecondaryKey();
+  }
+
+  deletePrimaryKey() {
+    this.primaryKey = '';
+    this.primaryKeyFile = '';
+    this.primaryKeyHeader = '';
+  }
+
+  deleteSecondaryKey() {
+    this.secondaryKey = '';
+    this.secondaryFile = '';
+    this.secondaryHeader = '';
   }
 }
