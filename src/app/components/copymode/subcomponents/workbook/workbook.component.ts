e63921e3b583e9deff36bc1ae5dd83dba6921cb6
Shelby McCowan <smccowan33@gmail.com>
2019-08-05 16:04:39 -0600

    Added new child components

---
+++ src/app/components/copymode/subcomponents/workbook/workbook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workbook',
+  templateUrl: './workbook.component.html',
+  styleUrls: ['./workbook.component.scss']
+})
+export class WorkbookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
