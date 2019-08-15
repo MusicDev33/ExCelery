bdf02fb6950cce3c239884ffc3f22e29b8147714
Shelby McCowan <smccowan33@gmail.com>
2019-08-15 11:13:10 -0600

    Control Panel is wired up for saving and copying

--- src/app/components/copymode/subcomponents/controlpanel/controlpanel.component.ts
+++ src/app/components/copymode/subcomponents/controlpanel/controlpanel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { CopyStoreService } from '../../../../providers/copymode/copystore.service';
+import { ASWorkbook } from '../../../../model/asworkbook';
 
 @Component({
   selector: 'app-controlpanel',
@@ -11,6 +12,20 @@ export class ControlPanelComponent implements OnInit {
 
   constructor(public store: CopyStoreService) { }
 
+  @Output()
+  save = new EventEmitter<ASWorkbook>();
+
   ngOnInit() {
+  }
+
+  // Wrapper functions
+  copyClicked() {
+    this.store.copyColumns();
+  }
+
+  saveClicked() {
+    const primaryFile = this.store.keyPair.primaryFile;
+    const primaryWorkbook = this.store.getWorkbookByFileName(primaryFile);
+    this.save.emit(primaryWorkbook);
   }
 }
